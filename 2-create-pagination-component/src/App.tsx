2f2e52acbca21c67fe691c4d2f471a76883c3e20
Yuki Yamamura <yuki.yamamura@icloud.com>
2023-04-02 14:48:56 +0900

    use pagination component in `App.tsx`

--- 2-create-pagination-component/src/App.tsx
+++ 2-create-pagination-component/src/App.tsx
@@ -1,23 +1,16 @@
+import Pagination from 'Pagination';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <div className="App-link">
+        <Pagination currentPageIndex={3} totalPosts={70} />
+      </div>
+    </header>
+  </div>
+);
 
 export default App;
