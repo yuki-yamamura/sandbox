d17659f8a0e1e4ea05305e0cdf7fc6e72987159d
Yuki Yamamura <yuki.yamamura@icloud.com>
2023-05-17 07:38:44 +0900

    try some actions and assertions

--- 6-cypress-getting-started/cypress/e2e/spec.cy.ts
+++ 6-cypress-getting-started/cypress/e2e/spec.cy.ts
@@ -1,5 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('My first test', () => {
+  it('gets, types and asserts', () => {
+    cy.visit('https://example.cypress.io');
+    cy.contains('type').click();
+
+    cy.url().should('include', '/commands/actions');
+
+    cy.get('.action-email').type('fake@email.com');
+    cy.get('.action-email').should('have.value', 'fake@email.com');
+  });
+});
