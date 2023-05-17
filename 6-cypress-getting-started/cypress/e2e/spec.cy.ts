e0985eec14d71f4dd27eba4454ebb7aac9ed4177
Yuki Yamamura <yuki.yamamura@icloud.com>
2023-05-18 06:42:25 +0900

    add new spec using Cypress Testing Library

--- 6-cypress-getting-started/cypress/e2e/spec.cy.ts
+++ 6-cypress-getting-started/cypress/e2e/spec.cy.ts
@@ -9,3 +9,16 @@ describe('My first test', () => {
     cy.get('.action-email').should('have.value', 'fake@email.com');
   });
 });
+
+describe('Using Cypress Testing Library', () => {
+  it('finds, types, and asserts', () => {
+    cy.visit('https://example.cypress.io');
+    cy.findAllByRole('link', { name: 'focus' }).click();
+
+    // focus password textarea
+    cy.findByPlaceholderText(/password/i).focus();
+
+    // then label text color should be orange
+    cy.findByText('Password').should('have.css', 'color', 'rgb(255, 165, 0)');
+  });
+});
