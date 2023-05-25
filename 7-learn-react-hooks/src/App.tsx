44032c1facdfddc2ca1b797866125c6ed7c39e39
Yuki Yamamura <yuki.yamamura@icloud.com>
2023-05-26 05:41:28 +0900

    learn memo API

--- 7-learn-react-hooks/src/App.tsx
+++ 7-learn-react-hooks/src/App.tsx
@@ -1,5 +1,35 @@
-import Todos from 'components/Todos';
+import { useState } from 'react';
+import Greeting from './components/Greeting';
 
-const App = () => <Todos />;
+const App = () => {
+  const [theme, setTheme] = useState<'red' | 'blue'>('red');
+  const [name, setName] = useState<string>();
+  const nextBackgroundColor = theme === 'red' ? 'blue' : 'red';
+
+  return (
+    <main className={`h-screen bg-${theme}-400`}>
+      <label htmlFor="theme-button">
+        {`Next background color is ${nextBackgroundColor}: `}
+        <button
+          id="theme-button"
+          type="button"
+          onClick={() => setTheme(nextBackgroundColor)}
+          className="bg-slate-200"
+        >
+          Change theme
+        </button>
+      </label>
+      <label htmlFor="name-textbox" className="block">
+        Name:{' '}
+        <input
+          id="name-textbox"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <Greeting name={name} />
+    </main>
+  );
+};
 
 export default App;
