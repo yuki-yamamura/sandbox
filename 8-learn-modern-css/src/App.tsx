d55f3393e5456c7c4c7910977f95e07aedc42366
Yuki Yamamura <yuki.yamamura@icloud.com>
2023-06-06 06:21:00 +0900

    write code using conventional CSS styles

--- 8-learn-modern-css/src/App.tsx
+++ 8-learn-modern-css/src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        {Array.from(new Array(100), (_, index) => index + 1).map((num) => (
+          <div className={num % 2 === 0 ? 'highlight' : ''}>item {num}</div>
+        ))}
+      </div>
     </div>
   );
 }
