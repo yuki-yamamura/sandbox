e03c571066556fc30b527bfaf9e5d4a660f2b4f6
Yuki Yamamura <yuki.yamamura@icloud.com>
2023-07-01 06:34:52 +0900

    initialize the project

---
+++ 9-learn-prisma/.eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  root: true,
+  rules: {
+    'padding-line-between-statements': [
+      'error',
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'return',
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used',
+        argsIgnorePattern: '_',
+        ignoreRestSiblings: false,
+        varsIgnorePattern: '_',
+      },
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        ts: 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+      },
+    },
+  },
+};
---
+++ 9-learn-prisma/src/index.ts
@@ -0,0 +1,4 @@
+import { sum } from './sum';
+
+// eslint-disable-next-line no-console
+console.log(sum(1, 2));
