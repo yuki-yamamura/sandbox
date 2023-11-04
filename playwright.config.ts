f50da8340ae789f22b8c1c7f0f57af66af1ddb1f
Yuki Yamamura <yuki.yamamura@icloud.com>
2023-11-05 06:16:40 +0900

    chore: revert playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,6 +20,18 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+    {
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 5'] },
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'] },
+    },
   ],
   webServer: {
     command: 'npm run dev',
