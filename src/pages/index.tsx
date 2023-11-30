bd57af8382b85be553b36b6da6963ccf93cbc528
Yuki Yamamura <yuki.yamamura@icloud.com>
2023-11-30 19:56:56 +0900

    chore: add user form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,5 @@
-import { schemaForType } from '@/validation';
-import { User } from '@prisma/client';
-import { z } from 'zod';
+import UserForm from '@/components/UserForm';
 
-import styles from './index.module.scss';
-
-const user: User = {
-  id: 1,
-  name: 'Bob',
-};
-
-const User = z.object({
-  id: z.number(),
-  name: z.string(),
-});
-
-User.parse(user);
-
-const userSchema = schemaForType<User>()(
-  z.object({
-    id: z.number(),
-    // name: z.string(),
-  }),
-);
-console.log(userSchema);
-
-const Page = () => (
-  <h1 className={styles.heading} data-cy="heading">
-    Hello, World!
-  </h1>
-);
+const Page = () => <UserForm />;
 
 export default Page;
