331162d4239e5b5de2144b5e10468fd17c46884b
Yuki Yamamura <yuki.yamamura@icloud.com>
2024-07-27 22:17:20 +0900

    learn useMutation hook

--- src/features/todos/components/Todos.tsx
+++ src/features/todos/components/Todos.tsx
@@ -1,28 +1,56 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { getTodos } from "../logic/getTodos";
+import { postTodo } from "../logic/postTodo";
+import { queryClient } from "@/lib/tanstack-query";
+import { useState } from "react";
+import { Todo } from "../types/Todo";
 
 const Todos = () => {
-  const { data: todos, isPending } = useQuery({
+  const [additionalTodos, setAdditionalTodos] = useState<Todo[]>([]);
+  const { data: fetchedTodos, isPending } = useQuery({
     queryKey: ["todos"],
     queryFn: getTodos,
   });
+  const { mutate: mutateTodos } = useMutation({
+    mutationFn: postTodo,
+    onSuccess: (todo) => {
+      setAdditionalTodos([todo, ...additionalTodos]);
+
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+    },
+  });
+  const todos = fetchedTodos?.concat(additionalTodos);
 
   if (isPending) {
     return <div>loading...</div>;
   }
 
-  if (!todos) {
+  if (!fetchedTodos) {
     return <div>something went wrong.</div>;
   }
 
   return (
-    <ul>
-      {todos.map((todo) => (
-        <li key={todo.id}>{todo.title}</li>
-      ))}
-    </ul>
+    <div>
+      <ul>
+        {todos?.map((todo, index) => (
+          <li key={index}>{todo.title}</li>
+        ))}
+      </ul>
+      <button
+        type="button"
+        onClick={() =>
+          mutateTodos({
+            userId: 1,
+            title: "foo",
+            completed: false,
+          })
+        }
+      >
+        Create new todo
+      </button>
+    </div>
   );
 };
 
