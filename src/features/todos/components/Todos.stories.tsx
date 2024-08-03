6e97cab98777e2deebbf53c7241d1dceb0d7aa2b
Yuki Yamamura <yuki.yamamura@icloud.com>
2024-08-03 14:03:18 +0900

    learn how to overwrite handler

--- src/features/todos/components/Todos.stories.tsx
+++ src/features/todos/components/Todos.stories.tsx
@@ -2,12 +2,38 @@ import type { Meta, StoryObj } from "@storybook/react";
 import Todo from "./Todos";
 import { handlers } from "@/mocks/handlers";
 import { QueryClientProvider } from "@/lib/tanstack-query";
+import { http, HttpResponse } from "msw";
+import { fakeTodos } from "@/mocks/todos/fakeData";
 
 const meta = {
   component: Todo,
   parameters: {
     msw: {
       handlers,
+    },
+  },
+} satisfies Meta;
+
+type Story = StoryObj<typeof Todo>;
+
+export const Default: Story = {
+  decorators: [
+    (Story) => (
+      <QueryClientProvider>
+        <Story />
+      </QueryClientProvider>
+    ),
+  ],
+};
+
+export const UncompletedTodos: Story = {
+  parameters: {
+    msw: {
+      handlers: [
+        http.get("https://jsonplaceholder.typicode.com/todos", () => {
+          return HttpResponse.json(fakeTodos.filter((todo) => !todo.completed));
+        }),
+      ],
     },
   },
   decorators: [
@@ -17,10 +43,6 @@ const meta = {
       </QueryClientProvider>
     ),
   ],
-} satisfies Meta;
-
-type Story = StoryObj<typeof Todo>;
-
-export const Default: Story = {};
+};
 
 export default meta;
